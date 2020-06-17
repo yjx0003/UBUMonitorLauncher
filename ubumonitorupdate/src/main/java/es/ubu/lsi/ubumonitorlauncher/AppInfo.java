5259e04ef98010c0caf8833aad18e4df7e970abc
Yi Peng <yipeng996@gmail.com>
2020-06-17 15:26:46 +0200

    Update log4j files configuration

--- ubumonitorupdate/src/main/java/es/ubu/lsi/ubumonitorlauncher/AppInfo.java
+++ ubumonitorupdate/src/main/java/es/ubu/lsi/ubumonitorlauncher/AppInfo.java
@@ -1,12 +1,18 @@
 package es.ubu.lsi.ubumonitorlauncher;
 
 public class AppInfo {
-
+	
+	public static final String VERSION = "0.1-stable"; 
+	public static final String APPLICATION_NAME = "UBUMonitor Launcher";
+	private static final String APPLICATION_NAME_WITH_VERSION = APPLICATION_NAME +" "+ VERSION;
 	public static final String CONFIGURATION_FILE = "configuration.json";
 	public static final String DEFAULT_VERSION_DIR = "./versions/";
 	public static final String DEFAULT_CHECK_URL = "https://raw.githubusercontent.com/yjx0003/UBUMonitor/master/autoUpdateConfig.json";
 	
 	public static final String ASK_AGAIN = "askAgain";
+	
+	public static final String LOGGER_FILE_APPENDER = "./log/" + APPLICATION_NAME_WITH_VERSION + ".log";
+
 	
 
 	private AppInfo() {
--- ubumonitorupdate/src/main/java/es/ubu/lsi/ubumonitorlauncher/UBUMonitorLauncher.java
+++ ubumonitorupdate/src/main/java/es/ubu/lsi/ubumonitorlauncher/UBUMonitorLauncher.java
@@ -1,10 +1,12 @@
 package es.ubu.lsi.ubumonitorlauncher;
 
 /**
- * UBUMonitorUpdate launcher: This just starts UBUMonitorUpdateMain because name consistent as other java applications.
+ * UBUMonitorUpdate launcher: This just starts UBUMonitorUpdateMain because name
+ * consistent as other java applications.
  */
 public class UBUMonitorLauncher {
 	public static void main(String[] args) {
+		System.setProperty("logfile.name", AppInfo.LOGGER_FILE_APPENDER);
 		Loader.initialize(args);
 	}
 
