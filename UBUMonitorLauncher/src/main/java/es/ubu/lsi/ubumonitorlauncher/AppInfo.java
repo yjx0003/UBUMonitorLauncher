404500a42d1107d916138916c2b2068998ebfeb3
Yi Peng <yipeng996@gmail.com>
2022-06-28 12:41:54 +0200

    Cambio de versión a laa 1.0.1

--- UBUMonitorLauncher/src/main/java/es/ubu/lsi/ubumonitorlauncher/AppInfo.java
+++ UBUMonitorLauncher/src/main/java/es/ubu/lsi/ubumonitorlauncher/AppInfo.java
@@ -8,7 +8,7 @@ package es.ubu.lsi.ubumonitorlauncher;
  */
 public class AppInfo {
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String APPLICATION_NAME = "UBUMonitor Launcher";
 	private static final String APPLICATION_NAME_WITH_VERSION = APPLICATION_NAME + " " + VERSION;
 
